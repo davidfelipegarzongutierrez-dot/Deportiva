6fa5b5793c93cd6a31ea6cbac3de14040b890004
Felipe <davidfelipegarzongutierrez@gmail.com>
2026-06-14 14:04:57 -0500

    Sistema local recuperado y migraciones corregidas

--- deportiva/settings.py
+++ deportiva/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qy84mr=9km-rzwa3d%hc%rtoil=zxrrk&uto8qjix-gn0^_081'
 
 # 🔥 PRODUCCIÓN (Railway)
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '*'
@@ -97,14 +97,29 @@ WSGI_APPLICATION = 'deportiva.wsgi.application'
 
 
 # ========================
-# BASE DE DATOS (RAILWAY POSTGRES)
+# BASE DE DATOS
 # ========================
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
-    )
-}
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+
+    DATABASES = {
+        'default': dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+
+else:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # ========================
